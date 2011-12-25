423efe4466860621a820c62bd3d16bcb472b04c7
Michael Evans <michaelcevans@me.com>
2011-12-25 18:55:10 -0500

    refactoring of sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,21 +1,23 @@
 class SmsController < ApplicationController
 
   def in
-    req = ActiveSupport::JSON.decode(request.body)
+    #test = JSON.parse(params)
+    logger.info { "TEST: #{params[:sm]}"}
+    #req = ActiveSupport::JSON.decode(params)
     #user = User.find_by_phone_number(params[:senderAddress])
-    #incoming_message = Smsified::IncomingMessage.new request
-    incoming_message2 = Smsified::IncomingMessage.new req
-    #logger.info { "SMS1: #{incoming_message['message']}"}
-    logger.info { "SMS2: #{incoming_message2['message']}"}
-    logger.info { "SMS Recieved  #{req}" }
-    logger.info { "Phone number: #{req['senderAddress']} with message #{req['message']}}"}
+    #incoming_message = Smsified::IncomingMessage.new params
+    #incoming_message2 = Smsified::IncomingMessage.new req
+    #logger.info { "SMS1: #{incoming_message}"}
+    logger.info { "SMS2: #{params}"}
+    #logger.info { "SMS Recieved  #{params[:sm][:inboundSMSMessage]}" }
+    #logger.info { "Phone number: #{req['senderAddress']} with message #{req['message']}}"}
     logger.info { "As Params Phone number: #{params[:senderAddress]} with message #{params[:message]}"}
-    if user
-      blip = user.blips.build(:message => params[:message])
-            #flit.message = flit.message[0..140]
-      blip.created_at = Time.now # HACK
-      blip.save!
-    end
+    #if user
+    #  blip = user.blips.build(:message => params[:message])
+    #        #flit.message = flit.message[0..140]
+    #  blip.created_at = Time.now # HACK
+    #  blip.save!
+    #end
   end
   
 end
