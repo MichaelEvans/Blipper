c5f442b25e3faa49b79ce64179f1f498a122115c
Michael Evans <michaelcevans@me.com>
2011-12-25 14:46:19 -0500

    now can follow and unfollow users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,4 +10,16 @@ class HomeController < ApplicationController
     @blips = @user.all_blips
   end
   
+  def toggle_follow
+    @user = User.find_by_username(params[:username])
+    if current_user.is_friend? @user
+      flash[:notice] = "You are no longer following @#{@user.username}"
+      current_user.remove_friend(@user)
+    else
+      flash[:notice] = "You are now following @#{@user.username}"
+      current_user.add_friend(@user)
+    end
+    redirect_to user_blips_path(@user.username)
+  end
+  
 end
