3a7b76a08020729bea733b0138ea7b713217990e
Michael Evans <michaelcevans@me.com>
2011-12-25 18:29:09 -0500

    testing smsified lib for controller

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,10 +1,11 @@
 class SmsController < ApplicationController
 
   def in
-    logger.info { "SMS Recieved #{request.body}" }
-    incoming_message = Smsified::IncomingMessage.new request.body
-    user = User.find_by_phone_number(params[:senderAddress])
+    req = ActiveSupport::JSON.decode(request.body)
+    #user = User.find_by_phone_number(params[:senderAddress])
     logger.info { "SMS Recieved  #{req}" }
+    logger.info { "Phone number: #{req['senderAddress']} with message #{req['message']}}"}
+    logger.info { "As Params Phone number: #{params[:senderAddress]} with message #{params[:message]}}"}
     if user
       blip = user.blips.build(:message => params[:message])
             #flit.message = flit.message[0..140]
