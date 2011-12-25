355d420139ff20b73cd6e8427f65771ffe69ff74
Michael Evans <michaelcevans@me.com>
2011-12-25 18:33:16 -0500

    refactoring of sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,9 +3,13 @@ class SmsController < ApplicationController
   def in
     req = ActiveSupport::JSON.decode(request.body)
     #user = User.find_by_phone_number(params[:senderAddress])
+    incoming_message = Smsified::IncomingMessage.new request
+    incoming_message2 = Smsified::IncomingMessage.new request.body
+    logger.info { "SMS1: #{incoming_message['message']}"}
+    logger.info { "SMS2: #{incoming_message2['message']}"}
     logger.info { "SMS Recieved  #{req}" }
     logger.info { "Phone number: #{req['senderAddress']} with message #{req['message']}}"}
-    logger.info { "As Params Phone number: #{params[:senderAddress]} with message #{params[:message]}}"}
+    logger.info { "As Params Phone number: #{params[:senderAddress]} with message #{params[:message]}"}
     if user
       blip = user.blips.build(:message => params[:message])
             #flit.message = flit.message[0..140]
