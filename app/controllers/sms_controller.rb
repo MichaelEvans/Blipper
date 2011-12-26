d7eb587cc3e76ff19a78cab4e3f18d870e5bd572
Michael Evans <michaelcevans@me.com>
2011-12-25 19:19:20 -0500

    refactoring of sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,13 +4,16 @@ class SmsController < ApplicationController
     #test = JSON.parse(params)
     sm = params[:sm]
     ismn = sm[:inboundSMSMessageNotification]
+    ism = ismn[:inboundSMSMessage]
+    message = ism[:message]
+    sender = ism[:senderAddress]
     logger.info { "M: #{JSON.pretty_generate(ismn)}"}
     #req = ActiveSupport::JSON.decode(params)
     #user = User.find_by_phone_number(params[:senderAddress])
     #incoming_message = Smsified::IncomingMessage.new params
     #incoming_message2 = Smsified::IncomingMessage.new req
     #logger.info { "SMS1: #{incoming_message}"}
-    logger.info { "SMS2: #{params}"}
+    logger.info { "SMS2: #{message} #{sender}"}
     #logger.info { "SMS Recieved  #{params[:sm][:inboundSMSMessage]}" }
     #logger.info { "Phone number: #{req['senderAddress']} with message #{req['message']}}"}
     logger.info { "As Params Phone number: #{params[:senderAddress]} with message #{params[:message]}"}
