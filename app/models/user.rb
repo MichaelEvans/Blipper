6c325b6bb35231efdbf056c3bb23325ba3aaeab4
Michael Evans <michaelcevans@me.com>
2011-12-27 19:05:25 -0500

    usernames are now case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   before_save :prepare_password
 
   validates_presence_of :username
-  validates_uniqueness_of :username, :email, :allow_blank => true
+  validates_uniqueness_of :username, :email, :allow_blank => true, :case_sensitive => false
   validates_format_of :username, :with => /^[-\w_@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_presence_of :password, :on => :create
